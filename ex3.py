051ce5e7f72f9d720db9674a6dddc61691ae2ebe
Nicole Kim <nkim1322@gmail.com>
2014-07-11 15:20:43 -0700

    Added ex3.py

---
+++ ex3.py
@@ -0,0 +1,27 @@
+# printing to the screen
+print "I will now count my chickens:"
+
+# printing to screen and number after (use comma)
+print "Hens" , 25 + 30 / 6
+print "Rooster", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+# math problem printed to screen (order of operations is PEMDAS)
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 <5-7?"
+
+print 3 + 2 < 5 - 7 
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5-7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+# after comment, expression using relational operators after comma states either true or false
+print "Is it greater?",  5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
