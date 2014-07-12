98451ea3cfb302ec0ed855727ebedca8d1dccd9e
Nicole Kim <nkim1322@gmail.com>
2014-07-11 23:47:24 -0700

    Updated ex3.py

--- ex3.py
+++ ex3.py
@@ -7,7 +7,6 @@ print "Rooster", 100 - 25 * 3 % 4
 
 print "Now I will count the eggs:"
 
-# math problem printed to screen (order of operations is PEMDAS)
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 print "Is it true that 3 + 2 <5-7?"
@@ -21,7 +20,6 @@ print "Oh, that's why it's False."
 
 print "How about some more."
 
-# after comment, expression using relational operators after comma states either true or false
 print "Is it greater?",  5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
