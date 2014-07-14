92438c980ab28de3c194e775d1752540893266ba
Nicole Kim <nkim1322@gmail.com>
2014-07-14 15:03:39 -0700

    Edited ex17.py and Added ex18.py
    ex18.py - functions

--- ex17.py
+++ ex17.py
@@ -5,8 +5,7 @@ script, from_file, to_file = argv
 
 print "Copying from %s to %s" % (from_file, to_file)
 
-indata = open(from_file).read()
-
+in_file = open(from_file); indata = in_file.read()
 
 print "The input file is %d bytes long" %len(indata)
 
