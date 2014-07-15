407e06295a093e1287d4bb0af27cbaf7a6d1a173
Nicole Kim <nkim1322@gmail.com>
2014-07-15 10:57:45 -0700

    Added ex39.py
    Hashmaps and dictionaries

---
+++ ex39_test.py
@@ -0,0 +1,34 @@
+import hashmap
+
+#debugging tests
+
+jazz = hashmap.new()
+hashmap.set(jazz, 'Miles Davis', 'Flamenco Sketches')
+#confirms set will replace previous one
+hashmap.set(jazz, 'Miles Davis', 'Kind of Blue')
+hashmap.set(jazz, 'Duke Ellington', 'Beginning to See the Light')
+hashmap.set(jazz, 'Billy Strayhorn', 'Lush Life')
+
+print "---- List Test ----"
+hashmap.list(jazz)
+
+print "---- Get Test ----"
+print hashmap.get(jazz, 'Miles Davis')
+print hashmap.get(jazz, 'Duke Ellington')
+print hashmap.get(jazz, 'Billy Strayhorn')
+
+print "---- Delete Test ----"
+print "**Goodbye Miles"
+hashmap.delete(jazz, "Miles Davis")
+hashmap.list(jazz)
+
+print "**Goodbye Duke"
+hashmap.delete(jazz, "Duke Ellington")
+hashmap.list(jazz)
+
+print "**Goodbye Billy"
+hashmap.delete(jazz, "Billy Strayhorn")
+hashmap.list(jazz)
+
+print "**Goodbye Pork Pie Hat"
+hashmap.delete(jazz, "Charles Mingus")
